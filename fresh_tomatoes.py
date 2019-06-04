a617a0370c430577268c58996a3d75585c8b33cc
Gloria Jimenez <gelenaj@gmail.com>
2019-06-04 07:57:53 -0400

    Add files via upload

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,20 +2,28 @@ import webbrowser
 import os
 import re
 
+
 # Styles and scripting for the page
+
 main_page_head = '''
+
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
+    <link href="https://fonts.googleapis.com/css?family=Ranga&display=swap" rel="stylesheet">
 
-    <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <style type="text/css" media="screen">
         body {
-            padding-top: 80px;
+            font-family: 'Ranga', cursive;
+            color:##FEFF0;
+        }
+        h2{
+            font-family: 'Roboto';
+            font-size: 10sp;
         }
         #trailer .modal-dialog {
             margin-top: 200px;
@@ -28,14 +36,17 @@ main_page_head = '''
             right: -12px;
             z-index: 9001;
         }
+
         #trailer-video {
             width: 100%;
             height: 100%;
         }
+
         .movie-tile {
             margin-bottom: 20px;
             padding-top: 20px;
         }
+
         .movie-tile:hover {
             background-color: #EEE;
             cursor: pointer;
@@ -44,6 +55,7 @@ main_page_head = '''
             padding-bottom: 56.25%;
             position: relative;
         }
+
         .scale-media iframe {
             border: none;
             height: 100%;
@@ -53,7 +65,9 @@ main_page_head = '''
             top: 0;
             background-color: white;
         }
+
     </style>
+
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
         $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
@@ -61,6 +75,7 @@ main_page_head = '''
             // reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
         });
+
         // Start playing the video whenever the trailer modal is opened
         $(document).on('click', '.movie-tile', function (event) {
             var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
@@ -72,22 +87,36 @@ main_page_head = '''
               'frameborder': 0
             }));
         });
+
         // Animate in the movies when the page loads
+
         $(document).ready(function () {
           $('.movie-tile').hide().first().show("fast", function showNext() {
             $(this).next("div").show("fast", showNext);
           });
+
         });
+
     </script>
+
 </head>
+
 '''
 
+
+
 # The main page layout and title bar
+
 main_page_content = '''
+
 <!DOCTYPE html>
+
 <html lang="en">
+
   <body>
+
     <!-- Trailer Video Modal -->
+
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -99,46 +128,53 @@ main_page_content = '''
         </div>
       </div>
     </div>
-    
-    <!-- Main Page Content -->
-    <div class="container">
-      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
+
+
+
+<!-- Main Page Content -->
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Ms. Jimenez's Favorite Movie Page</a>
+</nav>
+</div>
+    <div class="card-deck">
       {movie_tiles}
     </div>
   </body>
 </html>
+
 '''
 
+
+
 # A single movie entry html template
+
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <img class="card mx-auto" src="{poster_image_url}" width="220" height="342">
+    <h1>{movie_title}</h1>
+    <h2>{movie_storyline}</h2>
 </div>
 '''
+
+
 
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
         # Extract the youtube ID from the url
+
         youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
         trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
+
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            movie_storyline = movie.storyline
         )
     return content
 
@@ -149,10 +185,16 @@ def open_movies_page(movies):
   # Replace the placeholder for the movie tiles with the actual dynamically generated content
   rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
 
+
+
   # Output the file
+
   output_file.write(main_page_head + rendered_content)
   output_file.close()
 
+
+
   # open the output file in the browser
+
   url = os.path.abspath(output_file.name)
   webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
